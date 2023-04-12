2b680172f2e3534a9442af5b5d1f5c423021c5f3
Maks Malaniia <maks.malaniya@gmail.com>
2023-04-12 21:16:02 +0300

    added articles

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
+(function() {
+    const headerBurgerOpen = document.querySelector('.header__burger');
+    const headerNavigation = document.querySelector('.header__nav');
+    const headerBurgerCLose = document.querySelector('.header__nav-close');
+
+    headerBurgerOpen.addEventListener('click', event => {
+        headerNavigation.classList.add('active')
+    });
+
+    headerBurgerCLose.addEventListener('click', event => {
+        headerNavigation.classList.remove('active')
+    });
+})();
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     //direction: 'vertical',
