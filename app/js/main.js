5a56f344aec6a2f1a809b5ceb6e30b0570a18f30
Maks Malaniia <maks.malaniya@gmail.com>
2023-04-12 19:24:55 +0300

    added swiper and other changes

--- app/js/main.js
+++ app/js/main.js
@@ -2,20 +2,11 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     //direction: 'vertical',
     loop: true,
-  
+    spaceBetween: 30,
+    grabCursor: true,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+      clickable: true,
     },
   });
