3efa421b8db9316e4f6b1e28bfea8e11cca05258
Maks Malaniia <maks.malaniya@gmail.com>
2023-04-17 18:25:24 +0300

    added hovers

--- app/js/main.js
+++ app/js/main.js
@@ -12,6 +12,45 @@
     });
 })();
 
+(function() {
+    const languageItem = document.querySelectorAll('.header__lang-item');
+
+    languageItem.forEach((item, index) => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            languageItem.addEventListener('click', event => {
+                languageItem.classList.remove('active')
+            });
+
+            languageItem[index].classList.add('active');
+        })
+    })
+    
+})();
+
+(function() {
+    const tabItems = document.querySelectorAll('.experience__tab-link');
+    const tabContent = document.querySelectorAll('.experience__content-item');
+
+    tabItems.forEach((tab, index) => {
+        tab.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            tabContent.forEach(content => {
+                content.classList.remove('active');
+            });
+
+            tabItems.forEach(tab => {
+                tab.classList.remove('active');
+            });
+
+            tabContent[index].classList.add('active');
+            tabItems[index].classList.add('active')
+        })
+    })   
+})();
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     //direction: 'vertical',
